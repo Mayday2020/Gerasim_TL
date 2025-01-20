a08121a5b6e18f4540580bac430ebcc7968d20b5
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2025-01-20 19:31:22 +0300

    'unitTest'

---
+++ src/state/user-reducer.ts
@@ -0,0 +1,35 @@
+
+
+type StateType = {
+    age: number
+    childrenCount: number
+    name: string
+}
+type ActionType = {
+    type: string
+    [key: string]: any
+
+}
+
+export const userReducer = (state: StateType, action: ActionType): StateType => {
+    switch (action.type){
+        case 'INCREMENT-AGE': {
+            let newState = {...state}
+            newState.age = state.age + 1
+            return newState
+        }
+        case 'INCREMENT-CHILDREN-COUNT': {
+            let newState = {...state}
+            newState.childrenCount = state.childrenCount + 1
+            return newState
+        }
+        case 'CHANGE-NAME':
+            return {
+                ...state,
+                name: action.newName
+            }
+        case '':
+
+        default: throw new Error('OMG')
+    }
+}
